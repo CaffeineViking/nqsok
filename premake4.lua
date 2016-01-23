3aebbae7c20b9cded3b8a85f5bcff1ca04b2fb4d
Erik S. V. Jansson <caffeineviking@gmail.com>
2016-01-23 13:36:21 +0100

    Initial commit.

---
+++ premake4.lua
@@ -0,0 +1,29 @@
+#!lua
+
+local name = "nqsokoban"
+solution(name)
+    configurations({"debug", "release"})
+    location("build/")
+    targetdir("bin/")
+
+    language("C++")
+    flags("ExtraWarnings")
+    configuration("debug") flags("Symbols")
+    configuration("release") flags("Optimize")
+    configuration("gmake") buildoptions("-std=c++14 -Wextra -Wpedantic")
+    links({"png", "glfw", "GLEW", "GL"})
+    links({"vorbis", "ogg", "openal"})
+    links({"jsoncpp"})
+
+    project(name)
+        kind("WindowedApp")
+        files({"src/**.cc", "src/**.hh",
+               "src/**.cpp", "src/**.hpp", "src/**.h"})
+        excludes({"src/tests/**.cc", "src/tests/**.hh",
+                  "src/tests/**.cpp", "src/tests/**.hpp", "src/tests/**.h"})
+
+    project(name .. "_tests")
+        kind("ConsoleApp")
+        files({"src/**.cc", "src/**.hh",
+               "src/**.cpp", "src/**.hpp", "src/**.h"})
+        excludes("src/main.cc", "src/main.cpp")
